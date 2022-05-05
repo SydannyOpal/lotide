4c4c03c960078e0b6e7bbbd998f1da9e906ac9eb
SydannyOpal <sydanny15@gmail.com>
2022-05-05 02:34:55 -0400

    implementing eqObjects

---
+++ eqObjects.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqObjects = function(obj1, obj2) {
+  if (Object.keys(obj1).length === Object.keys(obj2).length) {
+    for (let key1 in obj1) {
+      if (obj1[key1] !== obj2[key2]) {
+        return false;
+      }
+      return true;
+    }
+  }
+  return false;
+};
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+eqObjects(ab, ba); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+eqObjects(ab, abc); // => false
